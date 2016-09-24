febe7733ef48477b7c78e1583c8e7908efdfbf2f
Ennio  Visconti <ennio.visconti@gmail.com>
2016-09-24 19:36:22 +0200

    Added Testing and Mocking Frameworks with an example test.

---
+++ test/MH-Engine.Tests/Controllers/HomeControllerTests.cs
@@ -0,0 +1,91 @@
+﻿using MH_Engine.Controllers;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace MH_Engine.Tests.UnitTests
+{
+    public class HomeControllerTests
+    {
+        [Fact]
+        public void Index_ReturnsAViewResult()
+        {
+            // Arrange
+            /*var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.ListAsync()).Returns(Task.FromResult(GetTestSessions()));
+            var controller = new HomeController(mockRepo.Object);*/
+            var controller = new HomeController();
+
+            // Act
+            var result = controller.Index();
+
+            // Assert
+            /*var viewResult = Assert.IsType<ViewResult>(result);
+            var model = Assert.IsAssignableFrom<IEnumerable<StormSessionViewModel>>(
+                viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());*/
+            Assert.IsType<ViewResult>(result);
+        }
+
+       /* [Fact]
+        public async Task IndexPost_ReturnsBadRequestResult_WhenModelStateIsInvalid()
+        {
+            // Arrange
+            var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.ListAsync()).Returns(Task.FromResult(GetTestSessions()));
+            var controller = new HomeController(mockRepo.Object);
+            controller.ModelState.AddModelError("SessionName", "Required");
+            var newSession = new HomeController.NewSessionModel();
+
+            // Act
+            var result = await controller.Index(newSession);
+
+            // Assert
+            var badRequestResult = Assert.IsType<BadRequestObjectResult>(result);
+            Assert.IsType<SerializableError>(badRequestResult.Value);
+        }
+
+        [Fact]
+        public async Task IndexPost_ReturnsARedirectAndAddsSession_WhenModelStateIsValid()
+        {
+            // Arrange
+            var mockRepo = new Mock<IBrainstormSessionRepository>();
+            mockRepo.Setup(repo => repo.AddAsync(It.IsAny<BrainstormSession>()))
+                .Returns(Task.CompletedTask)
+                .Verifiable();
+            var controller = new HomeController(mockRepo.Object);
+            var newSession = new HomeController.NewSessionModel()
+            {
+                SessionName = "Test Name"
+            };
+
+            // Act
+            var result = await controller.Index(newSession);
+
+            // Assert
+            var redirectToActionResult = Assert.IsType<RedirectToActionResult>(result);
+            Assert.Null(redirectToActionResult.ControllerName);
+            Assert.Equal("Index", redirectToActionResult.ActionName);
+            mockRepo.Verify();
+        }
+
+        private List<BrainstormSession> GetTestSessions()
+        {
+            var sessions = new List<BrainstormSession>();
+            sessions.Add(new BrainstormSession()
+            {
+                DateCreated = new DateTime(2016, 7, 2),
+                Id = 1,
+                Name = "Test One"
+            });
+            sessions.Add(new BrainstormSession()
+            {
+                DateCreated = new DateTime(2016, 7, 1),
+                Id = 2,
+                Name = "Test Two"
+            });
+            return sessions;
+        } */
+    }
+
+}
